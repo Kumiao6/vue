57b03f49616cf2231bbd85332d8f2e720f2a6b37
kumiaomiao”   git config --global user.email “78770486@qq.com <50994719+Kumiao6@users.noreply.github.com>
2022-04-27 17:12:38 +0800

    1

--- vue-admin-template-master/src/views/dict/list.vue
+++ vue-admin-template-master/src/views/dict/list.vue
@@ -1,45 +1,44 @@
 <template>
     <div class="app-container">
         <div class="el-toolbar">
-            <div class="el-toolbar-body" style="justify-content: flex-start;">
-                <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
-                    <el-button type="text"><i class="fa fa-plus"/> 导出</el-button>
-                </a>
+        <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
+                <el-button type="text"><i class="fa fa-plus"/> 导出</el-button>
+         </a>
                 <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
-            </div>
+
         </div>
+        
+    <el-table
+      :data="dictList"
+      style="width: 100%"
+      row-key="id"
+      border
+      lazy
+      :load="getChildrens"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <el-table-column label="名称" width="230" align="left">
+        <template slot-scope="scope">
+          <span>{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
 
-        <el-table
-        :data="list"
-        style="width: 100%"
-        row-key="id"
-        border
-        lazy
-        :load="getChildrens"
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <el-table-column label="编码" width="220">
+        <template slot-scope="{row}">
+          {{ row.dictCode }}
+        </template>
+      </el-table-column>
+      <el-table-column label="值" width="230" align="left">
+        <template slot-scope="scope">
+          <span>{{ scope.row.value }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.createTime }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
 
-            <el-table-column label="名称" width="230" align="left">
-            <template slot-scope="scope">
-            <span>{{ scope.row.name }}</span>
-            </template>
-            </el-table-column>
-            <el-table-column label="编码" width="220">
-            <template slot-scope="{row}">
-                    {{ row.dictCode }}
-            </template>
-            </el-table-column>
-            <el-table-column label="值" width="230" align="left">
-            <template slot-scope="scope">
-            <span>{{ scope.row.value }}</span>
-            </template>
-            </el-table-column>
-
-            <el-table-column label="创建时间" align="center">
-            <template slot-scope="scope">
-            <span>{{ scope.row.createTime }}</span>
-            </template>
-            </el-table-column>
-        </el-table>
 
         <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
             <el-form label-position="right" label-width="170px">
@@ -52,7 +51,7 @@
             :action="'http://localhost:8202/admin/cmn/dict/importData'"
             class="upload-demo">
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过500kb</div>
+            <div slot="tip" class="el-upload__tip">只能上传excel文件,且不超过500kb</div>
             </el-upload>
 
             </el-form-item>
@@ -64,6 +63,7 @@
             </el-button>
             </div>
         </el-dialog>
+
 
   </div>
 </template>
