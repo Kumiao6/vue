191faa3cd8b8abec21587a43753bd23135bae6b6
kumiaomiao”   git config --global user.email “78770486@qq.com <50994719+Kumiao6@users.noreply.github.com>
2022-05-03 21:02:02 +0800

    101

---
+++ demo/es6/vue_pro/nodejs/modularization_pro/src/01.js
@@ -0,0 +1,8 @@
+export default{
+    liist(){
+        console.log('list.....')
+    },
+    save(){
+        console.log('保存讲师')
+    }
+}
---
+++ demo/es6/vue_pro/webpackdemo/src/main.js
@@ -0,0 +1,5 @@
+const a = require('./01')
+const { add } = require('./02')
+const b = erquire('./02')
+
+a.info('hello' + add(1,2))
